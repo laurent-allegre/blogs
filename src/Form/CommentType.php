1112c65a0b3416aeb731e81ac93532de421e451a
laurent.allegre <laurent.allegre1@gmail.com>
2022-05-05 13:14:34 +0200

    Ajout d'evenenement sur le formulaire addEventListener

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -6,10 +6,18 @@ use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
 
 class CommentType extends AbstractType
 {
+    private $user;
+    public function __construct(Security $security)
+    {
+        $this->user = $security->getUser();
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -21,10 +29,18 @@ class CommentType extends AbstractType
                 'label' => "Votre nom"
             ])
             ->add('condition', CheckboxType::class, [
-                'mapped' => false
+                'mapped' => false,
+                'label' => "Accepter les conditions"
             ])
 
         ;
+        $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event){
+           $form = $event->getForm();
+           if ($this->user !== null){
+               $form->get('author')->setData($this->user->getUsername());
+           }
+
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
